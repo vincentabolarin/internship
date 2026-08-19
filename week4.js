48684fc6b4872ba13765c14c30c114dcad0fed5f
Vincent B. Abolarin <vincentab@ha-shem.com>
2026-08-19 12:24:46 +0100

    functions

--- week4.js
+++ week4.js
@@ -66,3 +66,94 @@ console.log('Is b equal to d?', b === d); // false
 const newObj = { plus: a + b, minus: a - b, times: a * b, divide: a / b };
 
 console.log(newObj);
+
+// Functions
+
+// declaration
+function showText() {
+  console.log('This is the text.');
+}
+
+showText();
+
+function greet(name) {
+  return `Hi ${name}`;
+}
+
+console.log(greet('Abdulrazaq'));
+
+// expression, stored in a variable
+const greet2 = function(name) {
+  return `Hi ${name}`;
+};
+
+console.log(greet2('Lawal'));
+
+// arrow function, the modern shorthand
+const greet3 = (name) => `Hi ${name}`;
+
+console.log(greet3("Tobiloba"));
+
+const greet4 = (name) => {
+  return `Hello, World and ${name}!`;
+}
+
+console.log(greet4('Jack'));
+
+function add(a, b, c = 4) {
+  console.log(a + b + c);
+}
+
+add(1, 2, 3);
+
+const newGreeting = (name = 'Samuel') => {
+  console.log(`My name is ${name}.`);
+}
+
+newGreeting('John');
+
+// if/else and Logical Operators
+const gradeStudent = (score) => {
+  let grade;
+
+  if (score >= 90) {
+    grade = "A";
+  } else if (score >= 70) {
+    grade = "B";
+  } else {
+    grade = "C";
+  }
+
+  console.log(`Student's Grade: ${grade}`);
+}
+
+gradeStudent(92);
+gradeStudent(48);
+gradeStudent(78);
+
+const checkGenderAndAge = (gender, age, isHungry) => {
+  if (gender === 'M' && age >= 18) {
+    console.log('You are good to go');
+  } else {
+    console.log('You are not good to go.');
+  }
+
+  if (gender === 'F' || age <= 13) {
+    console.log('Thank you.');
+  }
+
+  console.log(!isHungry);
+}
+
+checkGenderAndAge('F', 17, true);
+
+// for Loops
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+  // 0, 1, 2, 3, 4
+}
+
+for (let i = 0; i <= 5; i+2) {
+  console.log(i);
+  // 0, 1, 2, 3, 4, 5
+}
